db0bbfa15c7c63482bb49a1657a5d96a66fd6476
jaypark7821 <pjhyun1990@gmail.com>
2022-12-30 23:45:57 +0900

    docs add new requirements

--- src/test/java/com/jay/codingdojo/fizzbuzz/FizzBuzzConverterTest.java
+++ src/test/java/com/jay/codingdojo/fizzbuzz/FizzBuzzConverterTest.java
@@ -31,6 +31,10 @@ import org.junit.jupiter.api.Test;
  * 14
  * FizzBuzz
  * 16
+ *
+ * Stage2 - new requirements:
+ * A number is fizz if it is divisible by 3 or if it has a 3 in it
+ * A number is buzz if it is divisible by 5 or if it has a 5 in it
  */
 public class FizzBuzzConverterTest {
 

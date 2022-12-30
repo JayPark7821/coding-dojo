c21a6bb77035d79fb99b0873b4a8c78d8b3ef9b5
JayPark7821 <pjhyun1990@gmail.com>
2022-12-30 15:39:20 +0900

    feat car can move & show position

--- src/main/java/com/jay/codingdojo/carracing/Car.java
+++ src/main/java/com/jay/codingdojo/carracing/Car.java
@@ -3,11 +3,21 @@ package com.jay.codingdojo.carracing;
 public class Car {
 
 	private final String name;
+	private int position;
 
 	public Car(String name) {
 		if (name.length() > 5) {
 			throw new IllegalArgumentException("자동차 이름은 5자를 초과할 수 없습니다.");
 		}
 		this.name = name;
+		this.position = 0;
+	}
+
+	public void move() {
+		this.position++;
+	}
+
+	public String showPosition() {
+		return name + " : " + "-".repeat(position);
 	}
 }
--- src/test/java/com/jay/codingdojo/carracing/CarTest.java
+++ src/test/java/com/jay/codingdojo/carracing/CarTest.java
@@ -1,10 +1,20 @@
 package com.jay.codingdojo.carracing;
 
+import static org.assertj.core.api.AssertionsForClassTypes.*;
 import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+/**
+ * 기능 요구사항
+ *
+ * 각 자동차에 이름을 부여할 수 있다. 자동차 이름은 5자를 초과할 수 없다.
+ * 전진하는 자동차를 출력할 때 자동차 이름을 같이 출력한다.
+ * 자동차 이름은 쉼표(,)를 기준으로 구분한다.
+ * 전진하는 조건은 0에서 9 사이에서 random 값을 구한 후 random 값이 4 이상일 경우이다.
+ * 자동차 경주 게임을 완료한 후 누가 우승했는지를 알려준다. 우승자는 한 명 이상일 수 있다.
+ */
 class CarTest {
 
 	@Test
@@ -20,5 +30,13 @@ class CarTest {
 			"자동차 이름은 5자를 초과할 수 없습니다."
 		);
 	}
+
+	@Test
+	void carCanMove () throws Exception {
+		Car car = new Car("test");
+		car.move();
+		assertThat(car.showPosition()).isEqualTo("test : -");
+	}
+
 
 }

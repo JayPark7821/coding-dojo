504d9d1afd900487d3573c1400f85796d29dec93
JayPark7821 <pjhyun1990@gmail.com>
2022-12-30 15:34:17 +0900

    feat add validation for car name length

--- src/test/java/com/jay/codingdojo/carracing/CarTest.java
+++ src/test/java/com/jay/codingdojo/carracing/CarTest.java
@@ -2,13 +2,23 @@ package com.jay.codingdojo.carracing;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 class CarTest {
 
 	@Test
 	void canCreateCarWithName () throws Exception {
-	 	Car car = new Car("test");
+		assertDoesNotThrow(()->new Car("test"));
+	}
+
+	@Test
+	void carNameCantBeLongerThen5 () throws Exception {
+	    assertThrows(
+			IllegalArgumentException.class,
+			()->new Car("test12"),
+			"자동차 이름은 5자를 초과할 수 없습니다."
+		);
 	}
 
 }

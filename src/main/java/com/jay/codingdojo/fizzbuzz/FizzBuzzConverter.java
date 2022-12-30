86ce89b128d7407441afa6d7d81e42ada8edd741
jaypark7821 <pjhyun1990@gmail.com>
2022-12-30 23:29:44 +0900

    feat FizzBuzzConverter can convert multiples of Three to Fizz

--- src/main/java/com/jay/codingdojo/fizzbuzz/FizzBuzzConverter.java
+++ src/main/java/com/jay/codingdojo/fizzbuzz/FizzBuzzConverter.java
@@ -2,4 +2,10 @@ package com.jay.codingdojo.fizzbuzz;
 
 public class FizzBuzzConverter {
 
+	public String convert(int number) {
+		if(number % 3 == 0) {
+			return "Fizz";
+		}
+		return String.valueOf(number);
+	}
 }

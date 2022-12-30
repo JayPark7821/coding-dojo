f8eaff48c2064d4945f3017a2de3f4f8fdd619e0
jaypark7821 <pjhyun1990@gmail.com>
2022-12-30 21:02:04 +0900

    feat cars can move

--- src/main/java/com/jay/codingdojo/carracing2/Car.java
+++ src/main/java/com/jay/codingdojo/carracing2/Car.java
@@ -3,8 +3,18 @@ package com.jay.codingdojo.carracing2;
 public class Car {
 
 	private final String name;
+	private int position;
 
 	public Car(String carName) {
 		this.name = carName;
+		this.position = 0;
+	}
+
+	public void move() {
+		position++;
+	}
+
+	public int getCurrentPosition() {
+		return position;
 	}
 }
--- src/test/java/com/jay/codingdojo/carracing2/CarTest.java
+++ src/test/java/com/jay/codingdojo/carracing2/CarTest.java
@@ -1,13 +1,25 @@
 package com.jay.codingdojo.carracing2;
 
+import static org.assertj.core.api.AssertionsForClassTypes.*;
+
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 
 public class CarTest {
 
+	private Car sut;
+
+	@BeforeEach
+	void setUp() throws Exception {
+		sut = new Car("test");
+	}
+
 	@Test
-	void canCreateCars() throws Exception {
-		Assertions.assertDoesNotThrow(()->new Car("test"));
+	void carCanMove() throws Exception {
+		Assertions.assertDoesNotThrow(()-> sut.move());
+		assertThat(sut.getCurrentPosition()).isEqualTo(1);
+
 	}
 }

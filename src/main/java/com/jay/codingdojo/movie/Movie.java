51aaf1c40577b54bc6908d8e33d536208b986832
JayPark7821 <pjhyun1990@gmail.com>
2023-01-06 16:12:03 +0900

    feat should return average rating when rated more than 2 times

--- src/main/java/com/jay/codingdojo/movie/Movie.java
+++ src/main/java/com/jay/codingdojo/movie/Movie.java
@@ -1,20 +1,22 @@
 package com.jay.codingdojo.movie;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Movie {
 
 	private final String title;
-	private int rating;
+	private final List<Integer> rating = new ArrayList<>();
 
 	public Movie(String title) {
 		this.title = title;
-		this.rating = 0;
 	}
 
-	public int getRating() {
-		return this.rating;
+	public double getRating() {
+		return rating.stream().mapToInt(Integer::intValue).average().orElse(0);
 	}
 
 	public void rate(int rating) {
-		this.rating = rating;
+		this.rating.add(rating);
 	}
 }

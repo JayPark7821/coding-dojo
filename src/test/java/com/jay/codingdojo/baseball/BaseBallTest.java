9ecf2bd1b17db98024623194c7f773b98eb36c45
JayPark7821 <pjhyun1990@gmail.com>
2023-01-06 17:31:05 +0900

    feat can create BaseBall

---
+++ src/test/java/com/jay/codingdojo/baseball/BaseBallTest.java
@@ -0,0 +1,11 @@
+package com.jay.codingdojo.baseball;
+
+import org.junit.jupiter.api.Test;
+
+public class BaseBallTest {
+
+	@Test
+	void canCreateBaseBall () throws Exception {
+		new BaseBall();
+	}
+}

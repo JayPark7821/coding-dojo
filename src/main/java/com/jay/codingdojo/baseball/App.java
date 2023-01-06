6cc51f514e701c0ff5eea62dcf6f086529849ea2
jaypark7821 <pjhyun1990@gmail.com>
2023-01-07 01:14:48 +0900

    fix bugfix

--- src/main/java/com/jay/codingdojo/baseball/App.java
+++ src/main/java/com/jay/codingdojo/baseball/App.java
@@ -3,14 +3,14 @@ package com.jay.codingdojo.baseball;
 public class App {
 
 	private static final String GAME_START = "1";
-	private static final String GAME_END = "2";
+
 	public static void main(String[] args) {
 
 		InputView inputView = new InputView();
 		OutPutView outPutView = new OutPutView();
 		String gameStatus = GAME_START;
 
-		while (gameStatus == GAME_START) {
+		while (gameStatus.equals(GAME_START)) {
 			playBaseBallGame(inputView, outPutView);
 			gameStatus = inputView.shouldContinue();
 		}

7d64f073344ad705e2cb63d33b37eed261a48802
JayPark7821 <pjhyun1990@gmail.com>
2023-01-06 15:43:31 +0900

    feat add Movie

---
+++ src/main/java/com/jay/codingdojo/movie/Movie.java
@@ -0,0 +1,4 @@
+package com.jay.codingdojo.movie;
+
+public class Movie {
+}
---
+++ src/test/java/com/jay/codingdojo/movie/MovieTest.java
@@ -0,0 +1,11 @@
+package com.jay.codingdojo.movie;
+
+import org.junit.jupiter.api.Test;
+
+public class MovieTest {
+
+	@Test
+	void canCreateMovie () throws Exception {
+		new Movie();
+	}
+}

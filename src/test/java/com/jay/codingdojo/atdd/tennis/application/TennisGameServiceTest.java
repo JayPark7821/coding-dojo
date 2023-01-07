cead89c586f7c95d8bb015b257fb2af7b6420811
jaypark7821 <pjhyun1990@gmail.com>
2023-01-08 01:17:13 +0900

    refactor TennisGameServiceTest

--- src/test/java/com/jay/codingdojo/atdd/tennis/application/TennisGameServiceTest.java
+++ src/test/java/com/jay/codingdojo/atdd/tennis/application/TennisGameServiceTest.java
@@ -3,15 +3,23 @@ package com.jay.codingdojo.atdd.tennis.application;
 import static org.assertj.core.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import com.jay.codingdojo.atdd.tennis.service.TennisGameStatusResponse;
 
 class TennisGameServiceTest {
 
+	private TennisGameService sut;
+
+	@BeforeEach
+	void setUp() {
+		sut = new TennisGameService();
+	}
+
 	@Test
 	void create() {
-		assertThat(new TennisGameService().create())
+		assertThat(sut.create())
 			.isEqualTo(new TennisGameStatusResponse());
 	}
 

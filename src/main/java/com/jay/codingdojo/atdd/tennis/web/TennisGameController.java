fe7dd63a5011af4962e094e411e88b62808a4155
jaypark7821 <pjhyun1990@gmail.com>
2023-01-08 00:10:48 +0900

    간단한 예제라 컨트롤러 부터 연결하고 시작

--- src/main/java/com/jay/codingdojo/atdd/tennis/web/TennisGameController.java
+++ src/main/java/com/jay/codingdojo/atdd/tennis/web/TennisGameController.java
@@ -1,6 +1,7 @@
 package com.jay.codingdojo.atdd.tennis.web;
 
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,7 +14,16 @@ class TennisGameController {
 	@PostMapping
 	TennisGameStatusResponse create(){
 		throw new UnsupportedOperationException("unimplemented");
+	}
 
+	@PutMapping("/{id}/score/server")
+	TennisGameStatusResponse serverScores(){
+		throw new UnsupportedOperationException("unimplemented");
+	}
+
+	@PutMapping("/{id}/score/receiver")
+	TennisGameStatusResponse receiverScores(){
+		throw new UnsupportedOperationException("unimplemented");
 	}
 }
 

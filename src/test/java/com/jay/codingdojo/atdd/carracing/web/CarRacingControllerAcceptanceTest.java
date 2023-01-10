bc2e99d1f880ae38da9629553fc43858198c24b0
jaypark7821 <pjhyun1990@gmail.com>
2023-01-11 00:03:48 +0900

    test create racing game acceptance test

--- src/test/java/com/jay/codingdojo/atdd/carracing/web/CarRacingControllerAcceptanceTest.java
+++ src/test/java/com/jay/codingdojo/atdd/carracing/web/CarRacingControllerAcceptanceTest.java
@@ -1,4 +1,39 @@
 package com.jay.codingdojo.atdd.carracing.web;
 
+import static org.assertj.core.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import io.restassured.module.mockmvc.RestAssuredMockMvc;
+import io.restassured.module.mockmvc.response.ValidatableMockMvcResponse;
+import io.restassured.path.json.JsonPath;
+
 public class CarRacingControllerAcceptanceTest {
+
+	@BeforeEach
+	void setUp() throws Exception {
+		RestAssuredMockMvc.standaloneSetup();
+	}
+
+	@Test
+	@DisplayName("레이싱 게임을 생성한다.")
+	void create() {
+		final JsonPath response = RestAssuredMockMvc.
+
+			given()
+			.contentType("application/json").
+
+			when()
+			.post("/api/jay/car-racing").
+
+			then()
+			.statusCode(200)
+			.extract().jsonPath();
+
+		assertThat(response.getLong("id")).isEqualTo(1L);
+	}
+
+
 }

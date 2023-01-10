ddd0a15fcaea436be2f9f8b88e52ee93478e51fc
jaypark7821 <pjhyun1990@gmail.com>
2023-01-11 00:11:12 +0900

    test add acceptance test for adding cars to racing game

--- src/test/java/com/jay/codingdojo/atdd/carracing/web/CarRacingControllerAcceptanceTest.java
+++ src/test/java/com/jay/codingdojo/atdd/carracing/web/CarRacingControllerAcceptanceTest.java
@@ -2,6 +2,8 @@ package com.jay.codingdojo.atdd.carracing.web;
 
 import static org.assertj.core.api.Assertions.*;
 
+import org.json.JSONException;
+import org.json.JSONObject;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -19,7 +21,7 @@ public class CarRacingControllerAcceptanceTest {
 
 	@Test
 	@DisplayName("레이싱 게임을 생성한다.")
-	void create() {
+	void create() throws Exception {
 		final JsonPath response = RestAssuredMockMvc.
 
 			given()
@@ -34,6 +36,30 @@ public class CarRacingControllerAcceptanceTest {
 
 		assertThat(response.getLong("id")).isEqualTo(1L);
 	}
+
+	@Test
+	@DisplayName("레이싱 게임에 참여자를 추가한다.")
+	void addCars() throws Exception {
+		JSONObject carNames = new JSONObject();
+		carNames.put("carNames", "test1,test2,test3");
+
+		final JsonPath response = RestAssuredMockMvc.
+
+			given()
+			.contentType("application/json")
+			.body(carNames).
+
+			when()
+			.post("/api/jay/car-racing/1/car").
+
+
+			then()
+			.statusCode(200)
+			.extract().jsonPath();
+
+		assertThat(response.getString("3 Cars Participated")).isEqualTo("1");
+	}
+
 
 
 }

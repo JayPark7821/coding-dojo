95e7e473da27b69145454b90bd545592e95a1634
jaypark7821 <pjhyun1990@gmail.com>
2023-01-11 22:11:04 +0900

    test CarRacingControllerTest serivce mocking

--- src/test/java/com/jay/codingdojo/atdd/carracing/web/CarRacingControllerTest.java
+++ src/test/java/com/jay/codingdojo/atdd/carracing/web/CarRacingControllerTest.java
@@ -3,14 +3,28 @@ package com.jay.codingdojo.atdd.carracing.web;
 import static org.assertj.core.api.AssertionsForClassTypes.*;
 import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 
 import com.jay.codingdojo.atdd.carracing.service.CarRacingService;
 import com.jay.codingdojo.atdd.carracing.service.RaceStatusResponse;
 
+import static org.mockito.BDDMockito.given;
+
+@ExtendWith(MockitoExtension.class)
 class CarRacingControllerTest {
 
+	@Mock
 	private CarRacingService carRacingService;
+
+	@BeforeEach
+	void setUp() {
+		given(carRacingService.create())
+			.willReturn(new RaceStatusResponse(1L, null, null));
+	}
 
 	@Test
 	void create() {

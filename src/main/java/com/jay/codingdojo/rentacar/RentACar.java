869e5a8934e50a54a05523dd4688a6b9ec342092
JayPark7821 <pjhyun1990@gmail.com>
2023-01-13 13:49:27 +0900

    feat RentACar::addCar 구현 & 테스트 통과

--- src/main/java/com/jay/codingdojo/rentacar/RentACar.java
+++ src/main/java/com/jay/codingdojo/rentacar/RentACar.java
@@ -11,6 +11,6 @@ public class RentACar {
 	private List<Car> cars = new ArrayList<>();
 
 	public void addCar(Car car) {
-		throw new UnsupportedOperationException("com.jay.codingdojo.rentacar.RentACar.addCar())");
+		cars.add(car);
 	}
 }

ecfa0b5d7a9b71e3e8919aead91632884c1120de
JayPark7821 <pjhyun1990@gmail.com>
2023-01-17 18:58:07 +0900

    test CarRacingService::findById 테스트 케이스 추가

--- src/test/java/com/jay/codingdojo/atdd/carracing/service/CarRacingServiceTest.java
+++ src/test/java/com/jay/codingdojo/atdd/carracing/service/CarRacingServiceTest.java
@@ -3,9 +3,9 @@ package com.jay.codingdojo.atdd.carracing.service;
 import static org.assertj.core.api.AssertionsForClassTypes.*;
 import static org.mockito.BDDMockito.*;
 
-import java.util.List;
 import java.util.Optional;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -49,4 +49,31 @@ class CarRacingServiceTest {
 		assertThat(response).isEqualTo(new RaceStatusResponse(raceId, null, null, "2 Cars Participated"));
 	}
 
+	@Test
+	void can_find_by_id() throws Exception {
+		final Long raceId = 1L;
+
+		CarRacing carRacing = new CarRacing(raceId);
+
+		given(repository.findById(raceId))
+			.willReturn(Optional.of(carRacing));
+
+		CarRacing savedCarRacing = sut.findById(raceId);
+
+		assertThat(savedCarRacing).isEqualTo(carRacing);
+	}
+
+	@Test
+	void find_by_id_fail() throws Exception {
+		final Long raceId = 1L;
+
+		given(repository.findById(raceId))
+			.willReturn(Optional.empty());
+
+		Assertions.assertThrows(CarRacingNotFoundException.class,
+			() -> {
+				sut.findById(raceId);
+			}
+		);
+	}
 }

f4e228c78043138fe94e203d6240a8826e300b0a
JayPark7821 <pjhyun1990@gmail.com>
2023-01-17 18:47:57 +0900

    test CarRacingService::addCars 에서 findById메소드 제거로 인한 테스트 수정 & addCars_when_car_racing_not_found() 테스트 케이스 제거

--- src/test/java/com/jay/codingdojo/atdd/carracing/service/CarRacingServiceTest.java
+++ src/test/java/com/jay/codingdojo/atdd/carracing/service/CarRacingServiceTest.java
@@ -39,30 +39,10 @@ class CarRacingServiceTest {
 	}
 
 	@Test
-	void addCars_when_car_racing_not_found() throws Exception {
-		final Long raceId = 999L;
-
-		given(repository.findById(raceId))
-			.willReturn(Optional.empty());
-		CarRacing carRacingForCar = new CarRacing();
-
-		final CarRacingService sut = new CarRacingService(repository);
-
-		assertThatThrownBy(() -> sut.addCars(
-			carRacingForCar,
-			List.of(new Car(1L, "test1", carRacingForCar), new Car(2L, "test2", carRacingForCar)))
-		)
-			.isInstanceOf(CarRacingNotFoundException.class)
-			.hasMessage("Car Racing not found: " + raceId);
-	}
-
-	@Test
 	void addCars_success() throws Exception {
 		final Long raceId = 1L;
 
 		CarRacing carRacing = new CarRacing(raceId);
-		given(repository.findById(raceId))
-			.willReturn(Optional.of(carRacing));
 
 		final RaceStatusResponse response = sut.addCars(carRacing, Car.createCars("car1,car2", new CarRacing(1L)));
 

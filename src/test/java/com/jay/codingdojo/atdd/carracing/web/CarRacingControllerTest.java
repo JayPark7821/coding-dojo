a68f266d4ff2bc6f9df44e28dca19335efd4e194
JayPark7821 <pjhyun1990@gmail.com>
2023-01-18 13:46:45 +0900

    test CarRacingControllerTest 통합테스트 전환

--- src/test/java/com/jay/codingdojo/atdd/carracing/web/CarRacingControllerTest.java
+++ src/test/java/com/jay/codingdojo/atdd/carracing/web/CarRacingControllerTest.java
@@ -1,37 +1,25 @@
 package com.jay.codingdojo.atdd.carracing.web;
 
 import static org.assertj.core.api.AssertionsForClassTypes.*;
-import static org.junit.jupiter.api.Assertions.*;
-
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.Mock;
-import org.mockito.junit.jupiter.MockitoExtension;
-
-import com.jay.codingdojo.atdd.carracing.service.CarRacingService;
-import com.jay.codingdojo.atdd.carracing.service.CarRacingUsecase;
-import com.jay.codingdojo.atdd.carracing.service.RaceStatusResponse;
-
-import static org.mockito.BDDMockito.given;
 
 import java.util.List;
 import java.util.Map;
 
-@ExtendWith(MockitoExtension.class)
+import javax.transaction.Transactional;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.jdbc.Sql;
+
+import com.jay.codingdojo.atdd.carracing.service.RaceStatusResponse;
+
+@SpringBootTest
+@Transactional
 class CarRacingControllerTest {
 
-	@Mock
-	private CarRacingService carRacingService;
-	@Mock
-	private CarRacingUsecase carRacingUsecase;
+	@Autowired
 	private CarRacingController sut;
-
-
-	@BeforeEach
-	void setUp() {
-		sut = new CarRacingController(carRacingService,carRacingUsecase);
-	}
 
 	@Test
 	void create() {
@@ -39,30 +27,23 @@ class CarRacingControllerTest {
 		RaceStatusResponse expectedResponse
 			= new RaceStatusResponse(raceId, null, null, null);
 
-		given(carRacingService.create())
-			.willReturn(expectedResponse);
-
 		RaceStatusResponse response = sut.create();
 
 		assertThat(response).isEqualTo(expectedResponse);
 	}
 
-
 	@Test
+	@Sql("classpath:atdd/carracing/insert-carracing.sql")
 	void addCars() {
 
 		final Long raceId = 1L;
 		RaceStatusResponse expectedResponse
 			= new RaceStatusResponse(raceId, null, null, "3 Cars Participated");
 
-		given(carRacingUsecase.addCars(raceId, "test1,test2,test3"))
-			.willReturn(expectedResponse);
-
 		RaceStatusResponse response = sut.addCars(raceId, "test1,test2,test3");
 
 		assertThat(response).isEqualTo(expectedResponse);
 	}
-
 
 	@Test
 	void startRace() {
@@ -71,15 +52,10 @@ class CarRacingControllerTest {
 		RaceStatusResponse expectedResponse
 			= new RaceStatusResponse(raceId, null, null, "Race Started");
 
-		given(carRacingService.startRace(raceId))
-			.willReturn(expectedResponse);
-
 		RaceStatusResponse response = sut.startRace(raceId);
 
 		assertThat(response).isEqualTo(expectedResponse);
 	}
-
-
 
 	@Test
 	void getRaceWinner() {
@@ -87,9 +63,6 @@ class CarRacingControllerTest {
 		final Long raceId = 1L;
 		RaceStatusResponse expectedResponse
 			= new RaceStatusResponse(raceId, null, List.of("test1"), null);
-
-		given(carRacingService.getRaceWinner(raceId))
-			.willReturn(expectedResponse);
 
 		RaceStatusResponse response = sut.getRaceWinner(raceId);
 
@@ -102,9 +75,6 @@ class CarRacingControllerTest {
 		final Long raceId = 1L;
 		RaceStatusResponse expectedResponse
 			= new RaceStatusResponse(raceId, generateRaceHistory(), null, null);
-
-		given(carRacingService.getRaceHistory(raceId))
-			.willReturn(expectedResponse);
 
 		RaceStatusResponse response = sut.getRaceHistory(raceId);
 

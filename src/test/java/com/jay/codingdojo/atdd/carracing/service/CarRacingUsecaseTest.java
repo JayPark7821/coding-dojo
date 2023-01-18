6b6d23720dbe6d187c1c76f20eaa0be0657d0f33
JayPark7821 <pjhyun1990@gmail.com>
2023-01-18 13:40:05 +0900

    test CarRacingUsecaseTest를 통합 테스트로 전환

--- src/test/java/com/jay/codingdojo/atdd/carracing/service/CarRacingUsecaseTest.java
+++ src/test/java/com/jay/codingdojo/atdd/carracing/service/CarRacingUsecaseTest.java
@@ -1,49 +1,33 @@
 package com.jay.codingdojo.atdd.carracing.service;
 
 import static org.assertj.core.api.AssertionsForClassTypes.*;
-import static org.mockito.BDDMockito.*;
 
-import java.util.List;
-
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.Mock;
-import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.context.annotation.Import;
+import org.springframework.test.context.jdbc.Sql;
 
-import com.jay.codingdojo.atdd.carracing.domain.Car;
-import com.jay.codingdojo.atdd.carracing.domain.CarRacing;
+import com.jay.codingdojo.atdd.carracing.domain.CarRepository;
 
-@ExtendWith(MockitoExtension.class)
+@DataJpaTest
+@Import({
+	CarRacingUsecase.class,
+	CarRacingService.class,
+	CarService.class,
+	CarRepository.class
+})
 public class CarRacingUsecaseTest {
 
+	@Autowired
 	private CarRacingUsecase sut;
-	@Mock
-	private CarRacingService carRacingService;
-	@Mock
-	private CarService carService;
-
-	@BeforeEach
-	void setUp() {
-		sut = new CarRacingUsecase(carRacingService, carService);
-	}
 
 	@Test
+	@Sql("classpath:atdd/carracing/insert-carracing.sql")
 	void addCars() throws Exception {
 
 		final Long raceId = 1L;
-		CarRacing carRacing = new CarRacing(raceId);
-		List<Car> cars = List.of(new Car(1L, "car1", carRacing), new Car(2L, "car2", carRacing));
-
-		given(carRacingService.findById(raceId))
-			.willReturn(carRacing);
-		given(carService.create("car1,car2", carRacing))
-			.willReturn(cars);
-		given(carRacingService.addCars(carRacing, cars))
-			.willReturn(new RaceStatusResponse(raceId, null, null, "2 Cars Participated"));
-
 		final RaceStatusResponse response = sut.addCars(raceId, "car1,car2");
-
 		assertThat(response).isEqualTo(new RaceStatusResponse(raceId, null, null, "2 Cars Participated"));
 	}
 

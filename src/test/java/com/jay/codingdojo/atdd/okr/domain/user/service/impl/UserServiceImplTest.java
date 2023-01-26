e7651b07e5ef555705d7cfaaa6f5ec6415a4b47d
JayPark7821 <pjhyun1990@gmail.com>
2023-01-27 08:37:33 +0900

    test UserServiceImpl::loginWithIdToken testcase 추가

--- src/test/java/com/jay/codingdojo/atdd/okr/domain/user/service/impl/UserServiceImplTest.java
+++ src/test/java/com/jay/codingdojo/atdd/okr/domain/user/service/impl/UserServiceImplTest.java
@@ -42,35 +42,89 @@ class UserServiceImplTest {
 	@Test
 	@DisplayName("가입한 유저 정보가 없을 때  idToken을 통해 로그인을 시도하면 기대하는 응답(Guest)을 반환한다.")
 	void login_With_IdToken_when_before_join() throws Exception {
+
+		String userId = "testId";
+		String guestUuid = "guest_fewfewa3";
+		String guestName = "testUser";
+		String guestEmail = "test@email.com";
+		ProviderType provider = ProviderType.GOOGLE;
+		String guestPicture = "pic";
+
 		Guest guest = Guest.builder()
-			.guestId("testId")
-			.guestUuid("guest_fewfewa3")
-			.guestName("testUser")
-			.email("test@email.com")
-			.providerType(ProviderType.GOOGLE)
-			.profileImage("pic")
+			.guestId(userId)
+			.guestUuid(guestUuid)
+			.guestName(guestName)
+			.email(guestEmail)
+			.providerType(provider)
+			.profileImage(guestPicture)
 			.build();
 
 		given(googleTokenVerifier.varifyIdToken("idToken"))
-			.willReturn(new OAuth2UserInfo("testId", "testUser", "test@email.com", "pic"));
+			.willReturn(new OAuth2UserInfo(userId, guestName, guestEmail, guestPicture));
 
-		given(userRepository.findByEmail("test@email.com"))
+		given(userRepository.findByEmail(guestEmail))
 			.willReturn(Optional.empty());
 
-		given(guestRepository.findByGuestId("testId"))
+		given(guestRepository.findByGuestId(userId))
 			.willReturn(Optional.empty());
 
 		given(guestRepository.save(any(Guest.class)))
 			.willReturn(guest);
 
-		LoginInfo info = sut.loginWithIdToken(ProviderType.GOOGLE, "idToken");
+		LoginInfo info = sut.loginWithIdToken(provider, "idToken");
 
 		assertThat(info).isEqualTo(
 			new LoginInfo(
-				"guest_fewfewa3",
-				"test@email.com",
-				"testUser",
-				ProviderType.GOOGLE,
+				guestUuid,
+				guestEmail,
+				guestName,
+				provider,
+				null,
+				null
+			)
+		);
+	}
+
+	@Test
+	@DisplayName("회원 가입을 완료하지 않고 어플리케이션을 종료한 경우 idToken을 통해 로그인을 시도하면 새로운 응답(Guest)을 반환한다.")
+	void login_With_IdToken_when_temp_user() throws Exception {
+
+		String userId = "testId";
+		String guestUuid = "guest_fewfewa3";
+		String guestName = "testUser";
+		String guestEmail = "test@email.com";
+		ProviderType provider = ProviderType.GOOGLE;
+		String guestPicture = "pic";
+
+		Guest guest = Guest.builder()
+			.guestId(userId)
+			.guestUuid(guestUuid)
+			.guestName(guestName)
+			.email(guestEmail)
+			.providerType(provider)
+			.profileImage(guestPicture)
+			.build();
+
+		given(googleTokenVerifier.varifyIdToken("idToken"))
+			.willReturn(new OAuth2UserInfo(userId, guestName, guestEmail, guestPicture));
+
+		given(userRepository.findByEmail(guestEmail))
+			.willReturn(Optional.empty());
+
+		given(guestRepository.findByGuestId(userId))
+			.willReturn(Optional.of(mock(Guest.class)));
+		
+		given(guestRepository.save(any(Guest.class)))
+			.willReturn(guest);
+
+		LoginInfo info = sut.loginWithIdToken(provider, "idToken");
+
+		assertThat(info).isEqualTo(
+			new LoginInfo(
+				guestUuid,
+				guestEmail,
+				guestName,
+				provider,
 				null,
 				null
 			)

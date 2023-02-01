df33e677ffb08a61991207cbfc19e57579680e55
JayPark7821 <pjhyun1990@gmail.com>
2023-02-02 07:36:02 +0900

    test UserFacade::loginWithToken 테스트 통과 GoogleTokenVerifier mocking

--- src/test/java/com/jay/codingdojo/atdd/okr/application/user/UserFacadeTest.java
+++ src/test/java/com/jay/codingdojo/atdd/okr/application/user/UserFacadeTest.java
@@ -1,24 +1,36 @@
 package com.jay.codingdojo.atdd.okr.application.user;
 
 import static org.assertj.core.api.AssertionsForClassTypes.*;
+import static org.mockito.BDDMockito.*;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.annotation.Import;
 import org.springframework.test.context.jdbc.Sql;
 
 import com.jay.codingdojo.atdd.okr.domain.guest.service.GuestService;
+import com.jay.codingdojo.atdd.okr.domain.guest.service.impl.GuestServiceImpl;
 import com.jay.codingdojo.atdd.okr.domain.user.ProviderType;
 import com.jay.codingdojo.atdd.okr.domain.user.service.UserService;
+import com.jay.codingdojo.atdd.okr.domain.user.service.impl.UserServiceImpl;
+import com.jay.codingdojo.atdd.okr.interfaces.user.auth.GoogleTokenVerifier;
+import com.jay.codingdojo.atdd.okr.interfaces.user.auth.OAuth2UserInfo;
 
+@ExtendWith(MockitoExtension.class)
 @DataJpaTest
-@Import({UserFacade.class, UserService.class, GuestService.class})
+@Import({UserFacade.class, UserServiceImpl.class, GuestServiceImpl.class})
 public class UserFacadeTest {
 
 	@Autowired
 	private UserFacade sut;
+
+	@MockBean // InjectMocks
+	private GoogleTokenVerifier googleTokenVerifier;
 
 	@Test
 	@DisplayName("가입한 유저 정보가 없을 때  idToken을 통해 로그인을 시도하면 기대하는 응답(Guest)을 반환한다.")
@@ -28,6 +40,7 @@ public class UserFacadeTest {
 		String guestName = "testUser";
 		String guestEmail = "test@email.com";
 		ProviderType provider = ProviderType.GOOGLE;
+		stubGoogleTokenVerifier(userId, guestName, guestEmail, "picture");
 
 		LoginInfo info = sut.loginWithIdToken(provider, "idToken");
 
@@ -44,6 +57,7 @@ public class UserFacadeTest {
 		String guestName = "testUser";
 		String guestEmail = "test@email.com";
 		ProviderType provider = ProviderType.GOOGLE;
+		stubGoogleTokenVerifier(userId, guestName, guestEmail, "picture");
 
 		LoginInfo info = sut.loginWithIdToken(provider, "idToken");
 
@@ -56,6 +70,11 @@ public class UserFacadeTest {
 		assertThat(info.name()).isEqualTo(guestName);
 		assertThat(info.accessToken()).isNull();
 		assertThat(info.refreshToken()).isNull();
+	}
+
+	private void stubGoogleTokenVerifier(String userId, String userName, String userEmail, String userPicture) {
+		given(googleTokenVerifier.varifyIdToken("idToken"))
+			.willReturn(new OAuth2UserInfo(userId, userName, userEmail, userPicture, ProviderType.GOOGLE));
 	}
 
 }

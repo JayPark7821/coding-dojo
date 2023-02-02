6ed75bb7d0d3764735c0c27148ccb8ac68813316
jaypark7821 <pjhyun1990@gmail.com>
2023-02-02 20:14:36 +0900

    test 초최 회원가입시 임시 guestId return 인수테스트 통과

--- src/test/java/com/jay/codingdojo/atdd/okr/interfaces/user/UserApiControllerAcceptanceTest.java
+++ src/test/java/com/jay/codingdojo/atdd/okr/interfaces/user/UserApiControllerAcceptanceTest.java
@@ -1,17 +1,31 @@
 package com.jay.codingdojo.atdd.okr.interfaces.user;
 
 import static org.assertj.core.api.AssertionsForClassTypes.*;
+import static org.mockito.BDDMockito.*;
+
+import java.util.regex.Pattern;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.context.annotation.Import;
+
+import com.jay.codingdojo.atdd.okr.FakeTokenVerifier;
+import com.jay.codingdojo.atdd.okr.TestConfig;
+import com.jay.codingdojo.atdd.okr.domain.user.ProviderType;
+import com.jay.codingdojo.atdd.okr.interfaces.user.auth.GoogleTokenVerifier;
+import com.jay.codingdojo.atdd.okr.interfaces.user.auth.OAuth2UserInfo;
 
 import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
 import io.restassured.path.json.JsonPath;
 
+@Import(TestConfig.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class UserApiControllerAcceptanceTest {
 
@@ -29,6 +43,7 @@ public class UserApiControllerAcceptanceTest {
 	@Test
 	@DisplayName("가입한 유저 정보가 없을 때  idToken을 통해 로그인을 시도하면 기대하는 응답(Guest)을 반환한다.")
 	void login_With_IdToken_when_before_join() throws Exception {
+
 		final JsonPath response = RestAssured.
 
 			given()
@@ -41,12 +56,14 @@ public class UserApiControllerAcceptanceTest {
 			.statusCode(201)
 			.extract().jsonPath();
 
-		assertThat(response).hasNoNullFieldsOrPropertiesExcept(
-			"roleType",
-			"jobFieldDetail",
-			"accessToken",
-			"refreshToken"
+
+		assertThat(response.getString("guestId")).containsPattern(
+			Pattern.compile("guest-[a-zA-Z0-9]{14}")
 		);
-		assertThat(response.getString("guestUuid")).isNotBlank();
+		assertThat(response.getString("email")).isEqualTo(FakeTokenVerifier.email);
+		assertThat(response.getString("name")).isEqualTo(FakeTokenVerifier.name);
+		assertThat(response.getString("providerType")).isEqualTo(FakeTokenVerifier.provider.name());
+		assertThat(response.getString("accessToken")).isNull();
+		assertThat(response.getString("refreshToken")).isNull();
 	}
 }
